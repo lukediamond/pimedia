ac64519555f6d194de59d9f2f4b9402a78b443c5
lukediamond <lukediamond@protonmail.com>
2020-01-15 19:34:14 +0000

    Housekeeping

--- main.c
+++ main.c
@@ -20,71 +20,78 @@
 /* third-party includes */
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
-#include <wiringPi.h>
 
+/* audio playback constants */
 const int SAMPLE_RATE = 44100;
 const int CHUNK_SIZE = SAMPLE_RATE * 2;
-
-uint64_t nanos() {
-    struct timespec ts;
-
-    clock_gettime(CLOCK_REALTIME, &ts);
-    return ts.tv_sec * 1000000000 + ts.tv_nsec;
-}
 
 /*
  * PLAY THREAD
  */
 
+/* playthread variables */
 int playthread_active = 0;
 pthread_t playthread;
+uint64_t played_elapsed = 0;
 
+/* arguments to send to audio play thread */
 typedef struct {
-    int fd;
-    Mix_Chunk chunk;
-    int16_t* backbuf;
+    int         fd;      /* streaming file descriptor      */
+    Mix_Chunk   chunk;   /* frontbuffer and playable chunk */
+    int16_t*    backbuf; /* backbuffer samples             */
 } PlayThreadArgs;
 
-uint64_t played_elapsed = 0;
 void* playthread_func(void* pargs) {
     PlayThreadArgs* args = (PlayThreadArgs*) pargs;
 
+    /* 
+     * loop while thread is marked active 
+     * allows killing thread externally and gracefully 
+     */
     while (playthread_active) {
         int16_t* oldbuf;
         int numread;
 
+        /* read next chunk and store number of bytes read (in case 0 or less than chunk size) */
         numread = read(args->fd, args->backbuf, CHUNK_SIZE * sizeof(int16_t));
-        if (numread <= 0) break;
-        args->chunk.alen = numread;
-        if (!playthread_active) break;
+        /* kill thread on EOF, IO error, or inactive thread */
+        if (!playthread_active || numread <= 0) break;
+        /* swap front/back buffers and set played chunk size */
         oldbuf = (int16_t*) args->chunk.abuf;
+        args->chunk.alen = numread;
         args->chunk.abuf = (Uint8*) args->backbuf;
         args->backbuf = oldbuf;
+
+        /* play front buffer */
         Mix_PlayChannel(0, &args->chunk, 0);
+        /* wait for clip to finish */
         while (playthread_active && (Mix_Paused(0) || Mix_Playing(0))) {
             usleep(10000);
+            /* accumulate time if audio active */
             if (!Mix_Paused(0) && Mix_Playing(0))
                 played_elapsed += 10000;
         }
     } 
     playthread_active = 0;
 
+    /* thread returns nothing */
     return NULL;
 }
 
+/* helper function to stop thread */
 void playthread_stop() {
     if (playthread_active) {
+        /* mark thread as inactive and wait to exit */
         playthread_active = 0;
         pthread_join(playthread, NULL);
     }
 }
 
-void playthread_wait() {
-    pthread_join(playthread, NULL);
-}
-
+/* helper function to start thread */
 void playthread_start(PlayThreadArgs* args) {
+    /* stop thread if active */
     if (playthread_active) playthread_stop(); 
+    /* mark thread as active and create */
     playthread_active = 1;
     pthread_create(&playthread, NULL, playthread_func, args);
 }
@@ -94,21 +101,27 @@ void playthread_start(PlayThreadArgs* args) {
  */
 
 /* message types */
-const uint8_t MT_PLAY = 0;
-const uint8_t MT_PAUSE = 1;
-const uint8_t MT_RESUME = 2;
-const uint8_t MT_SEEK = 3;
-const uint8_t MT_GETELAPSED = 4;
+const uint8_t MT_PLAY        = 0;
+const uint8_t MT_PAUSE       = 1;
+const uint8_t MT_RESUME      = 2;
+const uint8_t MT_SEEK        = 3;
+const uint8_t MT_GETELAPSED  = 4;
 const uint8_t MT_GETDURATION = 5;
 
+/* play request fields */
 typedef struct {
+    /* file name string, max length of 128 */
     char filename[128];
 } Message_Play;
+/* seek request fields */
 typedef struct {
+    /* seek point in seconds, dword float */
     float timepoint;
 } Message_Seek;
 
+/* entry point */
 int main() {
+    /* forward-declare playthread arguments and socket file descriptor */
     PlayThreadArgs ptargs;  
     int sock;
 
@@ -127,6 +140,7 @@ int main() {
         /* allocate one audio channel */
         Mix_AllocateChannels(1);
 
+        /* initialize playthread arguments */
         ptargs.fd = -1;
         ptargs.chunk.allocated = 0;
         ptargs.chunk.abuf = (Uint8*) malloc(sizeof(int16_t) * CHUNK_SIZE);
@@ -173,76 +187,100 @@ int main() {
         uint8_t mtype;
         /* determine type of message (first byte) */
         read(remote, &mtype, 1);
-        /* handle message type, reading additional data */
+
+        /* handle play request */
         if (mtype == MT_PLAY) {
+            /* declare zero-initialized play request struct */
             Message_Play mesg = {0};
 
+            /* read play request struct from remote socket */
             read(remote, &mesg, sizeof(mesg));
             printf("received play request: %s\n", mesg.filename);
-            ptargs.fd = open(mesg.filename, O_RDONLY);  
-            if (ptargs.fd > 0)  {
-                struct stat st;
-                float duration;
 
-                fstat(ptargs.fd, &st);
-                duration = ((float) st.st_size / sizeof(int16_t)) / SAMPLE_RATE;
-                write(remote, &duration, 4);
+            /* attempt to open requested file */
+            ptargs.fd = open(mesg.filename, O_RDONLY);  
+            
+            /* handle valid file */
+            if (ptargs.fd > 0)  {
+                /* start play thread at time point 0 */
                 played_elapsed = 0;
                 playthread_start(&ptargs);
             }
             else {
+                /* print error on invalid file name */
                 printf("invalid file name: %s\n", mesg.filename);
             }
         }
+
+        /* handle pause request */
         if (mtype == MT_PAUSE) {
             printf("received pause request\n");
             /* pause audio if audio is playing */
             if (playthread_active)
                 Mix_Pause(0);
         }
+        
+        /* handle resume request */
         if (mtype == MT_RESUME) {
             printf("received resume request\n");
             /* resume audio if audio was playing */
             if (playthread_active)
                 Mix_Resume(0);
         }
+        
+        /* handle seek request */
         if (mtype == MT_SEEK) {
-            Message_Seek mesg;
+            /* zero-initialize seek request struct */
+            Message_Seek mesg = {0};
             
+            /* read request struct from remote socket */
             read(remote, &mesg, sizeof(mesg));
+
+            /* if current file descriptor is valid */
             if (ptargs.fd > 0) {
                 struct stat st;
                 uint32_t sample;
                 int waspaused;
 
+                /* get file size, compute sample */
                 fstat(ptargs.fd, &st);
                 sample = (uint32_t) (mesg.timepoint * SAMPLE_RATE);
                 if (sample >= st.st_size / sizeof(int16_t)) 
                     sample = st.st_size / sizeof(int16_t) - 1;
                 printf("received seek request to sample %u of %lu\n", sample, st.st_size / 2);
+                /* save current paused state, then force-pause stream */
                 waspaused = Mix_Paused(0);
                 Mix_Pause(0);
+                /* seek to correct byte in streaming file */
                 lseek(ptargs.fd, sample * sizeof(int16_t), SEEK_SET);
+                /* change elapsed time to requested time */
                 played_elapsed = (uint64_t) (1000000 * mesg.timepoint);
+                /* start thread if inactive and resume play */
                 if (!playthread_active)
                     playthread_start(&ptargs);
                 Mix_Resume(0);
-                if (Mix_Playing(0) || Mix_Paused(0))
-                    Mix_HaltChannel(0);
+
+                /* pause mix if it was playing before seeking */
                 if (waspaused) {
                     while (!Mix_Playing(0));
                     Mix_Pause(0);
                 }
             }
         }
+        
+        /* handle elapsed time query */
         if (mtype == MT_GETELAPSED) {
+            /* compute elapsed time in seconds, send as dword float */
             float elapsed = (float) played_elapsed / 1000000.0f;
             write(remote, &elapsed, 4);
         }
+
+        /* handle duration query */
         if (mtype == MT_GETDURATION) {
             struct stat st;
             float duration;
 
+            /* determine/send duration based on sample rate, sample size, and file size */
             fstat(ptargs.fd, &st);
             duration = (float) st.st_size / sizeof(int16_t) / SAMPLE_RATE;
             write(remote, &duration, 4);
--- serv.py
+++ serv.py
@@ -4,113 +4,160 @@ import urllib
 import socket
 import struct
 
+# decorator to handle socket connections
 def message(func):
     def wrapper(*args):
+        # initialize return value to none
         ret = None
         try:
+            # attempt to connect socket
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
             sock.connect(("0.0.0.0", 44))
+            # call the decorator's wrapped function, pass regular args + socket
             ret = func(sock, *args)
+            # close connection
             sock.close()
         except:
+            #handle connection error gracefully
             print("Failed to connect")
         return ret
     return wrapper
 
+# decorated function to send song play request (message type 0)
 @message
 def play_song(sock, songname):
     sock.send(struct.pack("B", 0) + songname.encode("utf-8"))
+
+# decorated function to send pause request (message type 1)
 @message
 def pause(sock):
     sock.send(struct.pack("B", 1))
+
+# decorated function to send song resume request (message type 2)
 @message
 def resume(sock):
     sock.send(struct.pack("B", 2))
+
+# decorated function to send seek request (message type 3)
 @message
 def seek(sock, timept):
+    # send type and time point in fractional seconds (float dword)
     sock.send(struct.pack("<Bf", 3, timept))
+
+# decorated function to query elapsed time (message type 4)
 @message
 def get_elapsed(sock):
+    # send query
     sock.send(struct.pack("B", 4))
-    resp = sock.recv(4)
-    if len(resp) < 4: return 0.0
-    return struct.unpack("<f", resp)[0]
-@message
-def get_duration(sock):
-    sock.send(struct.pack("B", 5))
+    # receive elapsed and return
     resp = sock.recv(4)
     if len(resp) < 4: return 0.0
     return struct.unpack("<f", resp)[0]
 
+# decorated function to query song duration (message type 5)
+@message
+def get_duration(sock):
+    # send query
+    sock.send(struct.pack("B", 5))
+    # receive duration and return
+    resp = sock.recv(4)
+    if len(resp) < 4: return 0.0
+    return struct.unpack("<f", resp)[0]
+
+# helper function to convert file extension to corresponding MIME type
 def mime(path):
     if path.endswith(".js"): return "text/javascript"
     if path.endswith(".html"): return "text/html"
     if path.endswith(".svg"): return "image/svg+xml"
     return ""
 
+# helper function to construct JSON list of available song paths
 def get_listing():
     return "[" + ",".join("\"" + x + "\"" for x in os.listdir("converted")) + "]"
 
 
+# UWSGI program entry point
+# env            - environment variables (CGI)
+# start_response - function to send HTTP response header 
 def application(env, start_response):
+    # fetch request method and URI from environment
     uri = env.get("REQUEST_URI", "")
     method = env.get("REQUEST_METHOD", "")
+    # default URI to index.html
     if uri == "/": uri = "/index.html"
 
+    # handle POST request
     if method == "POST":
+        # fetch length of payload for relavent request types
         contentlen = int("0" + env.get("CONTENT_LENGTH", ""))
+        # handle song list request
         if uri == "/getlist":
             start_response("200 OK", [("Content-Type", "text/json")])
             return [get_listing().encode("utf-8")]
+        # handle song play request
         if uri == "/playsong":
             songpath = env["wsgi.input"].read(contentlen)
             songlen = play_song(songpath)
             start_response("200 OK", [])
             return [str(songlen).encode("utf-8")]
+        # handle elapsed query
         if uri == "/getelapsed":
             start_response("200 OK", [("Content-Type", "text/plain")])
             return [str(get_elapsed()).encode("utf-8")]
+        # handle duration query
         if uri == "/getduration":
             start_response("200 OK", [("Content-Type", "text/plain")])
             return [str(get_duration()).encode("utf-8")]
+        # handle seek request
         if uri == "/seek":
             timept = float("0" + env["wsgi.input"].read(contentlen))
             seek(timept)
             start_response("200 OK", [])
             return [b""]
+        # handle resume request
         if uri == "/resume":
             resume()
             start_response("200 OK", [])
             return [b""]
+        # handle pause request
         if uri == "/pause":
             pause()
             start_response("200 OK", [])
             return [b""]
+        # handle file upload
         if uri == "/upload":
+            # create FieldStorage to facilitate file uploading
             form = cgi.FieldStorage(fp=env["wsgi.input"], environ=env)
+            # fetch name/data and compute appropriate pathnames
             fname = form["file"].filename
             fdata = form["file"].value
             tmpname = "tmp/" + fname
             cvtname = "converted/" + "".join(fname.split(".")[:-1]) + ".raw"
+            # write data to temporary media file
             with open(tmpname, "wb") as f:
                 f.write(bytes(fdata))
+            # run ffmpeg conversion from native format to 44.1khz 16-bit signed little-endian PCM
             os.system("ffmpeg -i \"" + tmpname + "\" -f s16le -acodec pcm_s16le -ar 44100 -ac 1 \"" + cvtname + "\"")
+            #remove temporary file
             os.remove(tmpname)
-
+            
+            # prevent form reload, redirect to main page immediately
             start_response("301 Moved Permanently", [("Location", "/")])
-            with open("index.html", "r") as f:
-                data = f.read()
             return [b""]
 
-
+    # handle GET request
     if method == "GET":
         try:
+            # read URI
             with open(uri[1:], "rb") as f:
                 data = f.read()
+            # send requested data with determined MIME type
             start_response("200 OK", [("Content-Type", mime(uri))]) 
             return data.encode("utf-8")
         except:
+            # handle GET error gracefully
             start_response("404 Not Found", [("Content-Type", "text/plain")])
             return []
+    # respond with empty message by default
     start_response("200 OK", [("Content-Type", "text/plain")])
     return []
